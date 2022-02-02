591e82a0d535f49803c7e9c820ba9ef20b496735
ibrahimKadri <78695030+ibrahimKadri@users.noreply.github.com>
2022-02-02 22:44:44 +0200

    Add files via upload

---
+++ RevealElementsOnPageScroll/js.js
@@ -0,0 +1,20 @@
+const reveal = gsap.utils.toArray('.reveal');
+reveal.forEach((text, i) => {
+    ScrollTrigger.create({
+        trigger: text,
+        toggleClass:'active',
+        start: 'top 90%',
+        end: 'top 20%',
+    })
+})
+
+
+const images = gsap.utils.toArray('img');
+images.forEach((img, i) => {
+    ScrollTrigger.create({
+        trigger: img,
+        toggleClass:'active',
+        start: 'top 90%',
+        end: 'top 20%',
+    })
+})
