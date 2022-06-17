009fb4b4d838e8e00690e5fc55262cd978d1e49e
ibrahimKadri <78695030+ibrahimKadri@users.noreply.github.com>
2022-06-17 10:28:23 +0300

    Add files via upload

---
+++ MemoryGame/js.js
@@ -0,0 +1,133 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+      {
+        name: 'fries',
+        img: 'images/fries.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+      },
+      {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+      },
+      {
+        name: 'pizza',
+        img: 'images/pizza.png'
+      },
+      {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+      },
+      {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+      },
+      {
+        name: 'fries',
+        img: 'images/fries.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+      },
+      {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+      },
+      {
+        name: 'pizza',
+        img: 'images/pizza.png'
+      },
+      {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+      },
+      {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+      }
+    ]
+  
+    cardArray.sort(() => 0.5 - Math.random())
+  
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let cardsWon = []
+  
+    //create your board
+    function createBoard() {
+      for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        grid.appendChild(card)
+      }
+    }
+  
+    //check for matches
+    function checkForMatch() {
+      const cards = document.querySelectorAll('img')
+      const optionOneId = cardsChosenId[0]
+      const optionTwoId = cardsChosenId[1]
+      
+      if(optionOneId == optionTwoId) {
+        document.getElementById('textMessage').innerHTML = " You have clicked the same image!"
+         setTimeout(function(){
+         document.getElementById("textMessage").innerHTML="";
+         },1000);
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+
+      }
+      else if (cardsChosen[0] === cardsChosen[1]) {
+        // alert('You found a match')
+        document.getElementById('textMessage').innerHTML = " You found a match!"
+         setTimeout(function(){
+         document.getElementById("textMessage").innerHTML="";
+         },1000);
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+      } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        // alert('Sorry, try again')
+        document.getElementById('textMessage').innerHTML = " Sorry, try again"
+         setTimeout(function(){
+         document.getElementById("textMessage").innerHTML="";
+         },1000);
+      }
+      cardsChosen = []
+      cardsChosenId = []
+      resultDisplay.textContent = cardsWon.length
+      if  (cardsWon.length === cardArray.length/2) {
+        document.getElementById('textMessage').innerHTML = " Congratulations! You found them all!"
+        setTimeout(function(){
+        document.getElementById("textMessage").innerHTML="";
+        },1000);
+      }
+    }
+  
+    //flip your card
+    function flipCard() {
+      let cardId = this.getAttribute('data-id')
+      cardsChosen.push(cardArray[cardId].name)
+      cardsChosenId.push(cardId)
+      this.setAttribute('src', cardArray[cardId].img)
+      if (cardsChosen.length ===2) {
+        setTimeout(checkForMatch, 200)
+        document.getElementById('textMessage').innerHTML="";
+      }
+    }
+  
+    createBoard()
+  })
+  
