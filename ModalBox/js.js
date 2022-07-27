9137f9b902a995466c9fe87791983055bd91910f
ibrahimKadri <78695030+ibrahimKadri@users.noreply.github.com>
2022-07-27 15:23:42 +0300

    Add files via upload

---
+++ ModalBox/js.js
@@ -0,0 +1,8 @@
+function clickedBtn() {
+    var modalBox = document.getElementById('modal');
+    modalBox.style.display="block";
+}
+function closeModal() {
+    var modalBox = document.getElementById('modal');
+    modalBox.style.display="none";
+}
