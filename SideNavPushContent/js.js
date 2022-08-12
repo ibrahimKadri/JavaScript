f455d99db179a634d50261dc5a98a2be6b34d5b2
ibrahimKadri <78695030+ibrahimKadri@users.noreply.github.com>
2022-08-12 10:24:54 +0300

    Add files via upload

---
+++ SideNavPushContent/js.js
@@ -0,0 +1,9 @@
+function openNav() {
+    var side_nav = document.getElementById('side_nav');
+    side_nav.style.width="100px";
+}
+function closeNav() {
+    var side_nav = document.getElementById('side_nav');
+    side_nav.style.width="0";
+
+}
