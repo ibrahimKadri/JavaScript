9284d4a54758fff814792a3aca55310a8f6e424c
ibrahimKadri <78695030+ibrahimKadri@users.noreply.github.com>
2022-12-25 17:59:13 +0200

    Add files via upload

---
+++ MultiStepForm/js.js
@@ -0,0 +1,31 @@
+var form1 = document.getElementById('form1');
+var form2 = document.getElementById('form2');
+var form3 = document.getElementById('form3');
+
+var next1 = document.getElementById('next1');
+var next2 = document.getElementById('next2');
+var back1 = document.getElementById('back1');
+var back2 = document.getElementById('back2');
+
+var progress = document.getElementById('progress');
+
+next1.onclick = function(){
+    form1.style.left = '-450px';
+    form2.style.left = '0px';
+    progress.style.width = '180px';
+}
+back1.onclick = function(){
+    form1.style.left = '0px';
+    form2.style.left = '450px';
+    progress.style.width = '90px';
+}
+next2.onclick = function(){
+    form2.style.left = '-450px';
+    form3.style.left = '0px';
+    progress.style.width = '270px';
+}
+back2.onclick = function(){
+    form2.style.left = '0px';
+    form3.style.left = '450px';
+    progress.style.width = '180px';
+}
